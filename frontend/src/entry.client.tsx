31af0f5ec6a2c809b63f1d95974274cead096e66
sixtyoneeightyjake <sixtyoneeightyjake@gmail.com>
2025-09-04 17:21:19 -0500

    Update entry.client.tsx

--- frontend/src/entry.client.tsx
+++ frontend/src/entry.client.tsx
@@ -11,6 +11,9 @@ import React, { startTransition, StrictMode } from "react";
 import { createRoot, hydrateRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import "./i18n";
+// Global styles (Tailwind + custom)
+import "./tailwind.css";
+import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import store from "./store";
 import { queryClient } from "./query-client-config";
